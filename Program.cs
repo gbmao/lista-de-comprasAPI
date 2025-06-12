4ac52bc60d62cd36c6158702a7c56adbc8bde410
gbmao <gabriel.machado.oli@gmail.com>
2025-06-12 14:24:21 -0300

    feat: ADD endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,46 @@
+using lista_de_comprasAPI.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<ItemDto> itens = [
+   new (1,"Arroz",2,new DateOnly(2030,01,30)),
+    new (2,"Feijão",1,new DateOnly(2020,05,12)),
+    new (3,"Macarrão",5,new DateOnly(2025,10,03))
+];
+
+//Get /itens
+app.MapGet("itens/", () => itens);
+
+//Get /itens/1
+
+app.MapGet("itens/{id}", (int id) =>
+{
+    var item = itens.Find(item => item.Id == id);
+    return item;
+});
+
+//POST /item/1
+
+app.MapPost("itens/", (CreateItemDto newItem) =>
+{
+    ItemDto item = new(
+        itens.Count + 1,
+        newItem.Name,
+        newItem.Quantities,
+        newItem.ExpirationDate);
+
+    itens.Add(item);
+
+    //return item.Id;
+});
+
+// DELETE delete itens/id
+
+app.MapDelete("itens/{id}", (int id) =>
+{
+    itens.RemoveAll(item => item.Id == id);
+    return Results.NoContent();
+});
 
 app.Run();
