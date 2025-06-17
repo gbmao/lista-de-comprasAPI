9a83f04ae4e5e9943939af16b0622184970a12ab
gbmao <gabriel.machado.oli@gmail.com>
2025-06-17 11:41:20 -0300

    feat: add data migrations

--- Data/Lista-de-comprasContext.cs
+++ Data/Lista-de-comprasContext.cs
@@ -1,8 +1,12 @@
 using System;
+using lista_de_comprasAPI.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace lista_de_comprasAPI.Data;
 
-public class Lista_de_comprasContext
+public class Lista_de_comprasContext(DbContextOptions<Lista_de_comprasContext> options) : DbContext(options)
 {
+    public DbSet<Item> Items => Set<Item>();
 
+    public DbSet<Category> Categories => Set<Category>();
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using lista_de_comprasAPI.Data;
 using lista_de_comprasAPI.Dtos;
 using lista_de_comprasAPI.Endpoints;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("Lista-de-comprasAPI");
+builder.Services.AddSqlite<Lista_de_comprasContext>(connString);
+
 var app = builder.Build();
 
 app.MapProgramEndpoint();
