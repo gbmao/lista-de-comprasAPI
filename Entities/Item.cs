9e38b3bed25598f049caca3921d99e4fbaadcc57
gbmao <gabriel.machado.oli@gmail.com>
2025-06-17 11:40:45 -0300

    fix: correct a problem with declarations

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -6,8 +6,8 @@ public class Item
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public required string Category { get; set; }
+    public  int CategoryId { get; set; }
     public required int Quantities { get; set; }
     public DateOnly ExpirationDate { get; set; }
-    public  Category? CategoryId { get; set; }
+    public  Category? Category { get; set; }
 }
